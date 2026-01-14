b6f4d5a15e22a6b6a40f7fabac068f70e4fca933
곽기진 <upper0house@gmail.com>
2026-01-14 16:19:07 +0900

    3

--- src/main/java/com/example/springbatchex/Customer.java
+++ src/main/java/com/example/springbatchex/Customer.java
@@ -1,11 +1,14 @@
 package com.example.springbatchex;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Customer {
 
+	private long id;
 	private String name;
-	private String year;
-	private int age;
+	private String age;
+
 }
---
+++ src/main/java/com/example/springbatchex/JsonConfiguration.java
@@ -0,0 +1,61 @@
+package com.example.springbatchex;
+
+import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.*;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class JsonConfiguration {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+
+	@Bean
+	public Job job(){
+		return jobBuilderFactory.get("batchJob")
+			.incrementer(new RunIdIncrementer())
+			.start(step1())
+			.build();
+	}
+
+	@Bean
+	public Step step1(){
+		return stepBuilderFactory.get("step1")
+			.<Customer,Customer>chunk(3)
+			.reader(customItemReader())
+			.writer(customItemWriter())
+			.build();
+	}
+	@Bean
+	public JsonItemReader<Customer> customItemReader(){
+		return new JsonItemReaderBuilder<Customer>()
+			.jsonObjectReader(new JacksonJsonObjectReader<>(Customer.class))
+			.resource(new ClassPathResource("customer.json"))
+			.name("jsonItemReader")
+			.build();
+	}
+
+	@Bean
+	public ItemWriter<Customer> customItemWriter() {
+		return items -> {
+			for (Customer item : items) {
+				System.out.println(item.toString());
+			}
+		};
+	}
+}
